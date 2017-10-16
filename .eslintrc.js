4fd390a31e7436e9449608c322fbb2b24ea37f98
Bashar Harfoush <bashar.harfoush@gmail.com>
2017-10-16 15:49:44 -0400

    use babel-eslint parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   extends: "airbnb",
-  env: {
-    browser: true
-  },
+  parser: "babel-eslint",
+  env: { browser: true },
   rules: {
+    strict: 0,
     "import/prefer-default-export": 1,
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
     "react/forbid-prop-types": 1,
--- js/containers/HouseMembersTable.js
+++ js/containers/HouseMembersTable.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import ReactTable from 'react-table';
+import PropTypes from 'prop-types';
 
 import getHouseMembersAPI from '../helpers/getHouseMembersAPI';
 
@@ -8,30 +8,32 @@ export default class HousesTable extends React.Component {
   constructor() {
     super();
     this.columns = [{
-        Header: 'Name',
-        accessor: 'name' // String-based value accessors!
-      }, {
-        Header: 'Gender',
-        accessor: 'gender',
-      },
-    ];
-    this.state = { data: [], pages: -1, loading: true };
+      Header: 'Name',
+      accessor: 'name',
+    }, {
+      Header: 'Gender',
+      accessor: 'gender',
+    }];
+    this.state = { data: [], loading: true };
   }
 
   componentDidMount() {
-    let houseId = this.props.match.params.id;
-    getHouseMembersAPI(houseId).then(data => {
+    const houseId = this.props.match.params.id;
+    getHouseMembersAPI(houseId).then((data) => {
       this.setState({ data, loading: false });
     });
   }
 
-  render = () => {
-    let houseId = this.props.match.params.id;
-    return <ReactTable
+  render = () =>
+    (<ReactTable
       loading={this.state.loading}
       data={this.state.data} // should default to []
       columns={this.columns}
       defaultPageSize={10}
-      className="-striped -highlight"/>;
-  }
+      className="-striped -highlight"
+    />);
 }
+
+HousesTable.propTypes = {
+  match: PropTypes.object.isRequired,
+};
